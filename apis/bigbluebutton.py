ce0347c75a162f3a15a2b46c1cbcb4bc2069bb6e
nghoa <nghoa@hotmail.de>
2020-10-13 16:25:39 +0200

    bbb api calls close to finished

--- apis/bigbluebutton.py
+++ apis/bigbluebutton.py
@@ -16,12 +16,48 @@ def get_meetings():
     if (xml_obj.messageKey):
         print('No active Meetings')
     else:
-        meetings = xml_obj.meetings
-        print(meetings)
+        meetings = xml_obj.find_all({"meeting"})     # meetings[<xml>, <xml>, <xml>, ...]
+        for meeting in meetings:
+            meeting = {
+                "meeting_name": meeting.meetingName.get_text(),
+                "meeting_id": meeting.meetingID.get_text(),
+                "internal_meeting_id": meeting.internalMeetingID.get_text(),
+                "voice_bridge": meeting.voiceBridge.get_text(),
+                "moderator_pw": meeting.moderatorPW.get_text()
+            }
+            print(meeting)
         
+'''
+    TODO
+        > add attendees to dictionary
+        ------------- Structure -------------------
+        attendee = {
+            "user_id": userID,
+            "full_name": fullName,
+            "role": role,                   ## If role === MODERATOR -> isPresenter also true
+        }
+'''
 
+def end_meeting(meeting_id, password):
+    query = 'end'
+    params = 'meetingID=' + url_encode(meeting_id) + '&password=' + url_encode(password)
+    # to calc the "checksum" we need to concatenate => query + params + S_KEY
+    checksum_string = query + params + S_KEY
+    checksum = sha1_hash(checksum_string)
+    api_request_string = DOMAIN + query + '?' + params + '&' +'checksum=' + checksum
+    print('full request: ' + api_request_string)
+    # Make API Call
+    response = requests.get(api_request_string)
+    xml_obj = Soup(response.content, 'xml')
+    return_code = xml_obj.returncode.get_text()
 
+    cases = {
+        "SUCCESS": "Das Meeting konnte erfolgreich beendet werden!",
+        "FAILED": "Das Meeting konnte nicht beendet werden"
+    }
 
+    # Check for correct return code
+    print (cases.get(return_code))
 
 
 '''
@@ -33,13 +69,13 @@ def get_meetings_req_string():
     query_string = 'getMeetings'
     query_string_append = query_string + S_KEY
     checksum = sha1_hash(query_string_append)
-    api_request_string = DOMAIN+query_string+'?checksum='+checksum
+    api_request_string = DOMAIN + query_string + '?' + 'checksum=' + checksum
     return api_request_string
 
 # Verwendung für andere API Request, welche _ Leerzeichen in den Params haben
 # TODO
 def url_encode(query):
-    return urllib.parse.quote(query)
+    return urllib.parse.quote_plus(query)
 
 def sha1_hash(string):
     encode_to_bytes = string.encode('utf-8')
@@ -48,7 +84,8 @@ def sha1_hash(string):
     return hashed_string
 
 def main():
-    get_meetings()
+    # get_meetings()
+    end_meeting('random 192592!', 'mp')
 
 if __name__ == '__main__':
     main()
