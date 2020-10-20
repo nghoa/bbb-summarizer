7edf5ae4ea740c729bc88c64d07c950b8e5fa4ea
nghoa <nghoa@hotmail.de>
2020-10-20 12:07:59 +0000

    added utility/mkdir everything

--- utility/convert_opus2wav.py
+++ utility/convert_opus2wav.py
@@ -1,17 +1,25 @@
-# import bigbluebutton api -> for internal_meeting_id
+import os
+import shutil
+from flask import jsonify
 
 '''
     !!! Only start the conversion of wav -> opus when meeting is finished!!!
 '''
 
-
 '''
     Needs to get "internalMeetingId" from /apis/bigbluebutton.py
 '''
-def get_opus():
-    internal_meeting_id = get_meeting_id()
-    path = f'/var/bigbluebutton/recording/raw{internal_meeting_id}/audio/{internal_meeting_id}.opus'
-    return path
+def get_opus(internal_meeting_id, path):
+    for paths, dirs, files in os.walk(path):
+        for file_ in files:
+            base, extension = file_.split('.')
+            if (extension == 'opus'):
+                # start conversion
+                audio_file = os.path.join(paths, file_)
+                test = opus_to_wav(audio_file)
+                print (base)
+                print (extension)
+                
 
 '''
     Opus -> Wav Converter
@@ -22,7 +30,14 @@ def opus_to_wav():
     # Save Wav.file in BBB filesystem
 
 def main():
-    pass
+    internal_meeting_id = 'd3b050f3b31a8b967b4affec4a6a044b31fbf0dc-1601799973266'
+    dev_dir = os.path.expanduser("~/dev")
+    dev_path = dev_dir + "/data/" + internal_meeting_id + "/audio/"
+    root_target_dir = os.path.dirname(dev_path)
+    opus = get_opus(internal_meeting_id, root_target_dir)
+
+if __name__ == '__main__':
+    main()
 
 
 
