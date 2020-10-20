63f2964614823ae25fb9599997c2d75d4f0199d5
nghoa <nghoa@hotmail.de>
2020-10-20 12:07:59 +0000

    added utility/mkdir everything

--- mk_folder.py
+++ mk_folder.py
@@ -2,6 +2,20 @@ import os
 import shutil
 from flask import jsonify
 
+# Getting dir for creation
+dev_dir = os.path.expanduser("~/dev")
+dev_path = dev_dir + "/data/"
+root_target_dir = os.path.dirname(dev_path)
+print('Root Target Dir: ' + root_target_dir)
+
+def main():
+    # TODO
+    internal_meeting_id = 'd3b050f3b31a8b967b4affec4a6a044b31fbf0dc-1601799973266'
+    # mkdir_data_folder()
+    # Check if meeting has ended!!!
+    # mkdir_presentation_folder(internal_meeting_id)
+    mkdir_audio_folder(internal_meeting_id)
+
 def mkdir_data_folder():
     access_rights = 0o755
     dev_dir = os.path.expanduser("~/dev")
@@ -9,7 +23,7 @@ def mkdir_data_folder():
     directory = os.path.dirname(path)
     if not os.path.exists(directory):
         try:
-            os.mkdir(path)
+            os.makedirs(path)
         except OSError:
             print("Creation of directory %s failed" % path)
         else:
@@ -17,16 +31,10 @@ def mkdir_data_folder():
     else:
         print("Directory already exists")
 
-def mkdir_presentation_folder():
+def mkdir_presentation_folder(internal_meeting_id):
     # TODO: Test internal_meeting_id where presentation was uploaded
-    internal_meeting_id = 'd3b050f3b31a8b967b4affec4a6a044b31fbf0dc-1601799973266'
     root_src_path = "/var/bigbluebutton/recording/raw/{}/presentation".format(internal_meeting_id)
-
-    # Getting dir for creation
-    dev_dir = os.path.expanduser("~/dev")
-    dev_path = dev_dir + "/data/"
-    root_target_dir = os.path.dirname(dev_path)
-    print('Root Target Dir: ' + root_target_dir)
+    root_replacement_path = "/var/bigbluebutton/recording/raw"
 
     # Get Move_Dir => Directory which contains files, where we want to move them out
     for o in os.listdir(root_src_path):
@@ -39,16 +47,12 @@ def mkdir_presentation_folder():
 
             # Get all files from presentation_dir 
             for src_dir, dirs, files in os.walk(root_src_dir):
-                # Use root_src_path NOT root_src_dir because there might be multiple presentation 
-                # folder structure: 
-                # <meeting_id_folder>
-                # ---- <presentation_id_folder_1>
-                # ---- <presentation_id_folder_2>
-                # ---- ....
-                dst_dir = src_dir.replace(root_src_path, root_target_dir)        
+                # Use root_replacement_path NOT root_src_dir because there might be multiple presentation 
+                dst_dir = src_dir.replace(root_replacement_path, root_target_dir)        # Test # root_src_dir
                 print("New Destination Dir: " + dst_dir)
                 if not os.path.exists(dst_dir):
-                    os.mkdir(dst_dir)
+                    os.makedirs(dst_dir)
+                # Simple Copy Paste into new folder
                 for file_ in files:
                     src_file = os.path.join(src_dir, file_)
                     dst_file = os.path.join(dst_dir, file_)
@@ -56,7 +60,34 @@ def mkdir_presentation_folder():
                         os.remove(dst_file)
                     shutil.copy(src_file, dst_dir)
 
-    return jsonify(status='success')
+    # TODO: Later for application 
+    # return jsonify({'status': 200})
+
+# Check audio structure
+def mkdir_audio_folder(internal_meeting_id):
+    root_src_path = "/var/bigbluebutton/recording/raw/{}/audio".format(internal_meeting_id)
+    root_replacement_path = "/var/bigbluebutton/recording/raw"
+    for src_dir, dirs, files in os.walk(root_src_path):
+        dst_dir = src_dir.replace(root_replacement_path, root_target_dir)        # Test # root_src_dir
+        # Make Audio folder if not existent
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+        for file_ in files:
+            src_file = os.path.join(src_dir, file_)
+            dst_file = os.path.join(dst_dir, file_)
+            if os.path.exists(dst_file):
+                os.remove(dst_file)
+            shutil.copy(src_file, dst_dir)
+
+    # TODO: Later for application 
+    # return jsonify({'status': 200})
+
+def mkdir_summarization_folder():
+    # TODO
+    # mkdir ~dev/<internal meeting id>/transcription
+    # mkdir ~dev/<internal meeting id>/alignment
+    # mkdir ~dev/<internal meeting id>/metadata.txt ???
+    pass
 
 if __name__ == '__main__':
-    mkdir_presentation()
+    main()
