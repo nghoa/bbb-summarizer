c64198ba447b67f1a3d457f870e7da232a28d9dd
nghoa <nghoa@hotmail.de>
2020-10-23 15:17:07 +0000

    adding ui loading screen for lectures

---
+++ app/static/js/spinner.js
@@ -0,0 +1,18 @@
+function spinnerOn() {
+    document.getElementById("overlay").style.display = "flex";
+}
+function spinnerOff() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+window.onload = function () {
+    if (status == 'loading') {
+        spinnerOn();
+    } else {
+        spinnerOff();
+    }
+    // TODO:
+    // dirty fix
+    window.location.href = "overview/loaded";
+}
+
