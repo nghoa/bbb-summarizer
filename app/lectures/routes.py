4c81189fa4648dad10e14dd56c0ffdfdbd987090
nghoa <nghoa@hotmail.de>
2020-10-24 10:42:29 +0000

    finished lecture params display integration

--- app/lectures/routes.py
+++ app/lectures/routes.py
@@ -1,39 +1,51 @@
 from flask import render_template, jsonify, request, redirect, url_for, json
 import time
-import datetime
+from datetime import datetime, timedelta
 # In App Modules
 from app.apis.bigbluebutton import get_meetings
 from . import lectures_blueprint
 from .get_stuff import get_config
 
-# TODO:
 # Querystring from confnum => get_meetings and so on...
 ## Getting Query String
 # data?param=value&param2=value2
 @lectures_blueprint.route('/lectures/data')
-def get_query_string():
-    # request.query_string           ## Whole Request String
+def get_meeting_info():
+    # Get all current meetings via API Call
     meetings = get_meetings()
     conf_num = request.args.get('confnum')
 
     for meeting in meetings:
+        # Voice Bridge used as identifier for "current meeting"
         if (meeting['voice_bridge'] == conf_num):
             internal_meeting_id = meeting['internal_meeting_id']
+            meeting_name = meeting['meeting_name']
+            current_presenter = meeting['current_presenter']
 
-    filepath = serve_filepath(internal_meeting_id)
+            # Dateformat transform: "1234567890" -> "dd-mm-YYYY hh:mm:ss" 
+            start_time_str = meeting['start_time']
+            f_start_number = float(start_time_str)
+            start_time = datetime.fromtimestamp(f_start_number / 1e3)
+            # original timezone was UTC -> +2 for Germany
+            german_start_time = (start_time + timedelta(hours=2)).strftime('%d-%m-%Y %H:%M:%S')
+            duration = int((datetime.now() - start_time).total_seconds() / 60)
 
-    metadata = {
-        "conference_number": conf_num,
-        "conference_name": request.args.get('confname'),
-        "internal_meeting_id": internal_meeting_id,
-        "filepath": filepath
-    }
-    return render_template('meta-data.html', metadata=metadata)
+            metadata_dict = {
+                "conference_number": conf_num,
+                "conference_name": meeting_name,            # request.args.get('confname') also possible
+                "internal_meeting_id": internal_meeting_id,
+                "start_time": german_start_time,
+                "current_presenter": current_presenter,
+                "current_duration": duration
+            }
+            metadata = json.dumps(metadata_dict)
+
+            return redirect(url_for('.show_lecture_with_metadata', metadata=metadata))
 
 
 # Testing out redirect from confnum -> lecture with metadata
 @lectures_blueprint.route('/lectures/testplace/data')
-def get_meeting_info():
+def get_meeting_info_test():
     conf_num = request.args.get('confnum')
     # TODO:
     # start_time transformation 
@@ -56,6 +68,9 @@ def get_meeting_info():
 def show_lecture_with_metadata():
     metadata = request.args['metadata']     # counterpart for url_for
     metadata_json = json.loads(metadata)
+
+    # TODO:
+    # Start EventListener: meeting_ended()? 
 
     return render_template('lecture_test.html', metadata = metadata_json)
 
